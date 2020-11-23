3d92e3ad4f86c2f71ae9637943bbc7eab047b259
Alejandro R. Mosteo <amosteo@unizar.es>
2020-11-23 18:34:00 +0100

    Fix build script

--- build.bash
+++ build.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Import already built rclada_alire workspace
 source $RCLADA_INSTALL_DIR/setup.bash
-colcon build
+
+# Prevent rebuilding already built dependencies
+touch alire/COLCON_IGNORE
+
+# Build only the packages in this workspace
+colcon build 
